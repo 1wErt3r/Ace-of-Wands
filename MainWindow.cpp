3b3eeed7023f4935593cfff840e58e78ae733bc2
politebot <root@politebot.com>
2025-08-13 07:58:04 -0500

    Window name change

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -24,7 +24,7 @@ const uint32 kMsgNewReading = 'NEW_';
 
 MainWindow::MainWindow()
 	:
-	BWindow(BRect(100, 100, 900, 700), "Tarot Card Reader", B_TITLED_WINDOW,
+	BWindow(BRect(100, 100, 900, 700), "Ace of Wands", B_TITLED_WINDOW,
 		B_ASYNCHRONOUS_CONTROLS | B_QUIT_ON_WINDOW_CLOSE),
 	fMenuBar(NULL),
 	fCardModel(NULL),
